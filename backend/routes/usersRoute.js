cbeff89f51e51ae64c9845680510fc2edb5ea4c6
unknown <hemanth66ps@gmail.com>
2024-05-21 14:03:24 +0530

    bcrypt

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/usersModel');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const authmiddleware = require('../middlewares/authmiddleware');
 
